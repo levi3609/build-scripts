512b964beaf78431969d757b3613f5742b871bbb
levi3609 <99073465+levi3609@users.noreply.github.com>
2024-02-01 13:41:42 +0600

    Update los20.sh

--- los20.sh
+++ los20.sh
@@ -10,5 +10,5 @@ rm -rf .repo
 rm -rf *
 repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 git clone https://github.com/levi3609/build-scripts.git -b los
-chmod u+x scripts/sync.sh
-./scripts/sync.sh
+chmod u+x build-scripts/sync.sh
+./build-scripts/sync.sh
