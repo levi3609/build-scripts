89fe65847e3ea77d3253972fe20a5ac18647a059
levi3609 <99073465+levi3609@users.noreply.github.com>
2024-02-01 11:39:33 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 # Clone the manifest repository
-git clone https://github.com/levi3609/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/LOS-Sweet/local_manifest --depth 1 -b los .repo/local_manifests
 
 # Force sync the repository
 repo sync --force-sync -c -j $(nproc)
