02575dc137ffe42a8753c6acef4391f92ffbec96
levi3609 <99073465+levi3609@users.noreply.github.com>
2024-02-01 12:17:06 +0600

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 rm -rf .repo/local_manifests
 
 # Clone the manifest repository
-git clone https://github.com/levi3609/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/LOS-Sweet/local_manifest --depth 1 -b los .repo/local_manifests
 
 # Start sync
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
